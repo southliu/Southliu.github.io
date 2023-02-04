9229abac834d39a4399ee473863b54d773001ea7
southliu <1275093225@qq.com>
2023-02-04 21:21:05 +0800

    test

---
+++ assets/js/components_Pagination.7d5ca9a2.js
@@ -0,0 +1,9 @@
+import{j as o}from"./components_Bottom.365a272e.js";import{r as t}from"./react.1c7e6da0.js";import{P as a}from"./antd.d2fd2e89.js";function s(i){return o("div",{id:"pagination",className:`
+        w-full
+        flex
+        items-center
+        justify-end
+        min-h-40px
+        box-border
+        z-999
+      `,children:o(a,{showSizeChanger:!0,showQuickJumper:!0,size:"small",showTotal:r=>`共 ${r||0} 条数据`,...i})})}const l=t.memo(s);export{l as B};
--- assets/js/page_demo_level1.92c98cec.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./components_Bottom.2d762bbc.js";import{u as e}from"./page_content_article.3c25fcd2.js";function i(){return e("三层结构"),r("div",{className:"m-30px",children:"三层结构"})}export{i as P};
