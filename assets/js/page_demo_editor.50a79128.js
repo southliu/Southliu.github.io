1ba38691d6e79448ba77327e76ec675a1f57fe0f
southliu <1275093225@qq.com>
2023-03-12 22:35:50 +0800

    perf: 抽离数据大屏

---
+++ assets/js/page_demo_editor.50a79128.js
@@ -0,0 +1,1 @@
+import{j as t}from"./components_Bottom.39240bb9.js";import{r as s}from"./react.4fb287ef.js";import{u as a}from"./page_content_article.819fb054.js";import{W as m}from"./components_WangEditor.051e2ad4.js";function u(){a("富文本");const[r,o]=s.useState("<p>hello</p>");return t("div",{className:"m-30px",children:t(m,{value:r,onChange:e=>o(e)})})}export{u as M};
--- assets/js/page_demo_level1.e3c1bbb1.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./components_Bottom.8e587700.js";import{u as e}from"./page_content_article.825bf483.js";function i(){return e("三层结构"),r("div",{className:"m-30px",children:"三层结构"})}export{i as P};
