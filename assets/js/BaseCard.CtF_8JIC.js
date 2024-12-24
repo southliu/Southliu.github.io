8a482e6a5f26b22ebf536b4d705b57ea56ae4afd
southliu <1275093225@qq.com>
2024-12-24 23:34:33 +0800

    perf; 手机端适配

--- assets/js/BaseCard.CtF_8JIC.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./react@18.2.0.CwGjMYfB.js";function r(r){const{children:e,className:o}=r;return n.jsx("div",{...r,id:"card",className:`\n        min-w-980px\n        h-full\n        box-border\n        overflow-auto\n        relative\n        box-border\n        px-5\n        py-3\n        rounded-5\n        ${o}\n      `,children:e})}export{r as B};
